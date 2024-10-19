9ce3069919193e9a3e476691492b38be100059a5
ali-ashan <aliahsanfardin@gmail.com>
2024-10-19 22:01:09 +0600

    solve problem: find positive, negetive and zero

---
+++ problem_solving/10.positive_negetive_number.py
@@ -0,0 +1,35 @@
+"""
+
+number = input("Enter your number: ")
+
+number = int(number)
+
+
+
+if number > 0:
+    print(f"{number} is positive number")
+elif number == 0:
+    print("The number is 0")
+else:
+    print(f"{number} is negetive number")
+
+"""
+
+
+
+number = input ("enter your num:")
+
+number = int(number)
+
+
+
+if number > 0:
+    print(f"{number}is positive number")
+
+elif number == 0 :
+    print ("the number is 0")
+
+else:
+    print(f"{number} is negetive number")
+
+
