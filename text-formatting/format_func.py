523785e13db061a7a1fa3072cdf5e855e85523f7
ali-ashan <aliahsanfardin@gmail.com>
2024-10-19 22:04:13 +0600

    practice: text formatting

---
+++ text-formatting/format_func.py
@@ -0,0 +1,12 @@
+number1 = 10
+
+number2 = 20
+
+number3 = 30
+
+
+# print("The print result: ", number1, number2, number3)
+
+# print("The 1st number ", number1, "The 2nd number", number2, "The 3nd number is ", number3)
+
+print("The 1st number {1} The 2nd number {2} The 3nd number is {0}".format(number1, number2, number3))
