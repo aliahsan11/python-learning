05912af5aa356893e778f117df1c9d6b27b2ea2c
ali-ashan <aliahsanfardin@gmail.com>
2024-10-24 21:12:45 +0600

    for loop practice

---
+++ looping/1.lopping.py
@@ -0,0 +1,6 @@
+# for i in range(1,100):
+
+#   print (i)
+
+for i in range (100,1, -1):
+    print(i)
---
+++ looping/for.py
@@ -0,0 +1,22 @@
+
+# for i in range(0,100):
+#     print("hello Fardin! : ", i+1)
+
+for i in range(1,101):
+    if i % 2 == 0:
+        print("the even is ",i)
+    
+
+    else:
+        print("the odd number is",i)
+
+
+    
+
+ 
+
+
+
+
+
+
