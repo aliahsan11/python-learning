de0187fb82ffc021d4d64ff5f54ea2dec1c40393
ali-ashan <aliahsanfardin@gmail.com>
2024-10-25 19:01:48 +0600

    New : problem_solving\Even Or Odd number chek

---
+++ problem_solving/1.even_odd_num _chek.py
@@ -0,0 +1,11 @@
+while True:
+    num = input("enter your number :")
+    num = float(num)
+
+
+    if  num % 2 ==0:
+        print('your number is even')
+
+
+    else :
+        print("your number is odd")
