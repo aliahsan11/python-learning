d257987e3ef8860e8e41dfb9263993f1089efa30
ali-ashan <aliahsanfardin@gmail.com>
2025-01-25 20:43:59 +0600

    check divider

---
+++ problem_solving/22.chek_divider.py
@@ -0,0 +1,10 @@
+while True:
+
+    number = input(" Enter your number ")
+    number = int(number)
+
+    if number % 5 == 0:
+        print("the number divided by 5")
+
+    else:
+        print("the number not divided by 5")
