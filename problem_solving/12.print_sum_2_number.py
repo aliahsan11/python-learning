bed0f636ab2aa68d774776fb68538b8d63d9cc65
ali-ashan <aliahsanfardin@gmail.com>
2025-01-25 20:35:17 +0600

    problem: sum 2 number with user input.

---
+++ problem_solving/12.print_sum_2_number.py
@@ -0,0 +1,9 @@
+number_1 = input(" Enter your number 1 :")
+number_2 = input(" Enter your number 2 :")
+
+number_1 = int(number_1)
+number_2 = int(number_2)
+print(type(number_1))
+
+number_1 = number_1 + number_2
+print("sum", number_1)
